1848f78ffba4aebb8ae74b37380c8ebeb0f326cd
Sushma R Kashyap <sushma@nb673.SALINDIA.AD>
2018-07-12 17:37:31 +0530

    ui changes

--- server.py
+++ server.py
@@ -5,6 +5,15 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
-
+@app.route('/contactme', methods=['POST'])
+def contactme():
+    print(request.form['fullname'])
+    print(request.form['email'])
+    return jsonify({
+        "fullName":request.form['fullname'],
+        "email":request.form['email']
+    })
+     
+    
 if __name__=='__main__':
     app.run(debug=True)
